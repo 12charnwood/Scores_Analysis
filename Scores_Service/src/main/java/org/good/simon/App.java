abf03201ac0f31ac9cc1d45bb200db17c84222d7
simon <simongood@hotmail.com>
2020-12-19 22:50:44 +0000

    README

---
+++ Scores_Service/src/main/java/org/good/simon/App.java
@@ -0,0 +1,13 @@
+package org.good.simon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
